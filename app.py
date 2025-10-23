40cae8294afa6d459aeddc97a2e6df4f653f949b
Lê Thiên An <161805046+annc19324@users.noreply.github.com>
2025-10-23 23:32:37 +0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, send_file, render_template
+import yt_dlp
+import os
+
+app = Flask(__name__)
+
+# Cấu hình thư mục tĩnh để phục vụ hình ảnh
+app.config['UPLOAD_FOLDER'] = 'images'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/images/<filename>')
+def serve_image(filename):
+    return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+@app.route('/download', methods=['POST'])
+def download():
+    url = request.form['url']
+    try:
+        ydl_opts = {
+            'outtmpl': 'downloads/%(title)s.%(ext)s',
+            'format': 'best',
+            'noplaylist': True  # Chỉ tải video đơn, bỏ qua playlist
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            filename = ydl.prepare_filename(info)
+        
+        return send_file(filename, as_attachment=True, download_name=os.path.basename(filename))
+    except Exception as e:
+        return render_template('index.html', error=str(e))
+
+if __name__ == '__main__':
+    if not os.path.exists('downloads'):
+        os.makedirs('downloads')
+    if not os.path.exists('images'):
+        os.makedirs('images')
+    app.run(debug=True)
